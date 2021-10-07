843c7f5bf1f0edd5d08b70749787bc4fc154c950
TheMostPrettyStudent <90366780+TheMostPrettyStudent@users.noreply.github.com>
2021-10-07 10:44:34 +0800

    Update fetch_input.c

--- fetch_input.c
+++ fetch_input.c
@@ -8,16 +8,16 @@ int main() {
     int d = 0;
     int e = 0;
 
-    printf("½Ð¿é¤J¤­­Ó¼Æ¦r¡A¥HªÅ¥Õ¹j¶}¡C\n");
+    printf("è«‹è¼¸å…¥äº”å€‹æ•¸å­—ï¼Œä»¥ç©ºç™½éš”é–‹ã€‚\n");
     //add scanf code to get data from input
     //and feed those input into a, b, c, d, e, respectively.
     //only ONE line code is you need
     //please refer page 18 of slides
-    while (scanf("%d%d%d%d%d", &a, &b, &c, &d, &e)) {
+
+    scanf("%d%d%d%d%d", &a, &b, &c, &d, &e);
         printf("a=%d\n", a);
         printf("b=%d\n", b);
         printf("c=%d\n", c);
         printf("d=%d\n", d);
         printf("e=%d\n", e);
-    }
 }
